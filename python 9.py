1021be36a97e6bf49a24667a8f9a4a09d190280e
graj04 <115605326+graj04@users.noreply.github.com>
2022-12-14 23:36:08 +0530

    Add files via upload

---
+++ python 9.py
@@ -0,0 +1,14 @@
+#object oriented programme
+student1 = {"name":"gaurav","marks":50}
+student2 = {"name":"ajay","marks":40}
+print()
+
+class Person:
+    pass
+
+p = Person()
+print(p)
+
+a =1 
+def square(a):
+    print(a**2)
